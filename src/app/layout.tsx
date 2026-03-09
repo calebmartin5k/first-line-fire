85447f1b548ce2b9b1421ba0aab469d2a837a02b
calebmartin5k <accounts@conklinmedia.com>
2026-03-09 15:05:06 -0400

    new metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'My Google AI Studio App',
+  title: 'First-Line Fire Extinguisher Co.',
   description: 'First-Line Fire Extinguisher Co.',
 };
 
